d2bb9547317fc515d5441ab70d75719bd05ba1e0
Micho Garcia <michogar@gmail.com>
2014-10-28 13:05:50 +0100

    Added OTP client to gh-pages

---
+++ otp.config.js
@@ -0,0 +1,16 @@
+
+var OTP_config = {
+	
+	"initLatLng" : [41.376164507321796,2.1153831481933594],
+
+	"osmMapKey": "conveyal.ikck6888", // temporary -- do not use in production, provide your own
+	"aerialMapKey": "conveyal.map-a3mk3jug", // unset
+
+	"otpApi": "http://api.tmb.cat/v1/planner",
+    "otpApiKey" : "c1708a798d0c879002694a90b825937a",
+    "otpAppId" : "f7b34f42",
+	"esriApi": "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/",
+
+	"reverseGeocode": false
+
+};
