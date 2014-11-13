d77b01796201a07cbdd261ddc0cdeea6d76ab914
Oscar Fonts <oscar.fonts@gmail.com>
2014-11-13 11:55:34 +0100

    Add OTP service URLs for local testing environments

--- otp.config.js
+++ otp.config.js
@@ -1,3 +1,8 @@
+var urls = {
+	local_jar: "http://localhost:8080/otp/routers/default", // local 0.11.x otp.jar
+	local_war: "http://localhost:8080/otp-rest-servlet/ws/routers/default", // local 0.11.x otp-rest-servlet.war
+	tmb_api: "http://api.tmb.cat/v1/planner" // Public API TMB
+};
 
 var OTP_config = {
 	
@@ -6,7 +11,7 @@ var OTP_config = {
 	"osmMapKey": "conveyal.ikck6888", // temporary -- do not use in production, provide your own
 	"aerialMapKey": "conveyal.map-a3mk3jug", // unset
 
-	"otpApi": "http://api.tmb.cat/v1/planner",
+	"otpApi": urls.tmb_api,
     "otpApiKey" : "c1708a798d0c879002694a90b825937a",
     "otpAppId" : "f7b34f42",
 	"esriApi": "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/",
